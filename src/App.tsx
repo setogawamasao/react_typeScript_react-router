dc95c28f50b8c8ca09a2741ce26a34e4107d18b3
masaosetogawa <ms.job.hunting@gmail.com>
2020-03-22 20:14:52 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  withRouter,
+  RouteComponentProps
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
-  );
+  </BrowserRouter>
+);
+
+interface IProps extends RouteComponentProps<{}> {}
+
+class Home extends React.Component<IProps, {}> {
+  constructor(props: IProps) {
+    super(props);
+    this.handleToAbout = this.handleToAbout.bind(this);
+  }
+
+  handleToAbout() {
+    this.props.history.push("/about");
+  }
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <input type="button" value="about" onClick={this.handleToAbout}></input>
+      </div>
+    );
+  }
+}
+class About extends React.Component<IProps, {}> {
+  constructor(props: IProps) {
+    super(props);
+    this.handleToHome = this.handleToHome.bind(this);
+  }
+  handleToHome() {
+    this.props.history.push("/");
+  }
+  render() {
+    const ChildWithRouter = withRouter(Child);
+    return (
+      <div>
+        <h2>About</h2>
+        <input type="button" value="home" onClick={this.handleToHome}></input>
+        <ChildWithRouter />
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component<IProps, {}> {
+  constructor(props: IProps) {
+    super(props);
+    this.handleToHome = this.handleToHome.bind(this);
+  }
+  handleToHome() {
+    this.props.history.push("/");
+  }
+  render() {
+    return (
+      <div>
+        <h2>Child</h2>
+        <input type="button" value="home" onClick={this.handleToHome}></input>
+      </div>
+    );
+  }
 }
 
 export default App;
