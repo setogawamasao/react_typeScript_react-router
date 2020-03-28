cd54d336a30b923fe2e43af715d730f0eb4fdb61
masaosetogawa <ms.job.hunting@gmail.com>
2020-03-28 19:33:48 +0900

    add text box

--- src/App.tsx
+++ src/App.tsx
@@ -17,38 +17,68 @@ const App = () => (
 );
 
 interface IProps extends RouteComponentProps<{}> {}
+interface IState {
+  value: string;
+}
 
-class Home extends React.Component<IProps, {}> {
+class Home extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.handleToAbout = this.handleToAbout.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleToAbout() {
-    this.props.history.push("/about");
+    this.props.history.push({
+      pathname: "/about",
+      state: { value: this.state.value }
+    });
   }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <h2>Home</h2>
+        <div>
+          <input type="text" onChange={this.handleChange}></input>
+        </div>
         <input type="button" value="about" onClick={this.handleToAbout}></input>
       </div>
     );
   }
 }
-class About extends React.Component<IProps, {}> {
+class About extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
+    this.state = { value: (this.props.location.state as IState).value };
     this.handleToHome = this.handleToHome.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
   handleToHome() {
     this.props.history.push("/");
   }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     const ChildWithRouter = withRouter(Child);
     return (
       <div>
         <h2>About</h2>
+        <div>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
+        </div>
         <input type="button" value="home" onClick={this.handleToHome}></input>
         <ChildWithRouter />
       </div>
@@ -60,14 +90,26 @@ class Child extends React.Component<IProps, {}> {
   constructor(props: IProps) {
     super(props);
     this.handleToHome = this.handleToHome.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleToHome() {
     this.props.history.push("/");
+  }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
   }
   render() {
     return (
       <div>
         <h2>Child</h2>
+        <div>
+          <input
+            type="text"
+            value={(this.props.location.state as IState).value}
+            onChange={this.handleChange}
+          ></input>
+        </div>
         <input type="button" value="home" onClick={this.handleToHome}></input>
       </div>
     );
